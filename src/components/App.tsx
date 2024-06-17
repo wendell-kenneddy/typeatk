c6e1f5f797672904f6f290ddf94eb94e23dff8bf
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-17 17:51:02 -0300

    feat(App): update boilerplate code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,16 @@
+import { CombatScreen } from "./CombatScreen";
+import { DifficultySelect } from "./DifficultySelect";
 import { Header } from "./Header";
 
 export function App() {
   return (
     <>
       <Header />
+
+      <main>
+        {/* <DifficultySelect /> */}
+        <CombatScreen totalTime={1000 * 30} />
+      </main>
     </>
   );
 }
