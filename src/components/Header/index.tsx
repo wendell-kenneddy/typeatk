bb82c4bd74c767a2172b4fd99ffc38ec196e901f
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-17 17:47:57 -0300

    feat(Header): use mantine components to compose the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import styles from "./header.module.css";
+import { Center, Title } from "@mantine/core";
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <h1>TypeATK</h1>
-      </div>
-    </header>
+    <Center component="header" w="100%" py="sm" bg="dark">
+      <Title size="h3" order={1}>
+        typeATK
+      </Title>
+    </Center>
   );
 }
