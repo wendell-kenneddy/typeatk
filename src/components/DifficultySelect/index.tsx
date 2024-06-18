239642a8e8b1951cfa12fba74f8f85fb9f8accd2
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-18 13:42:05 -0300

    feat(DifficultySelect): handle difficulty selection and start game

--- src/components/DifficultySelect/index.tsx
+++ src/components/DifficultySelect/index.tsx
@@ -1,5 +1,7 @@
 import { Button, ComboboxData, NativeSelect, VisuallyHidden } from "@mantine/core";
+import { useState } from "react";
 import styles from "./DifficultySelect.module.css";
+import { Difficulty } from "../../data/difficultyModifiers";
 
 const difficulties: ComboboxData = [
   { label: "Easy", value: "easy" },
@@ -7,7 +9,17 @@ const difficulties: ComboboxData = [
   { label: "Hard", value: "hard" },
 ];
 
-export function DifficultySelect() {
+interface DifficultySelectProps {
+  onConfirmDifficulty: (s: Difficulty) => void;
+}
+
+export function DifficultySelect({ onConfirmDifficulty }: DifficultySelectProps) {
+  const [difficulty, setDifficulty] = useState<Difficulty>("normal");
+
+  function handleConfirmDifficulty() {
+    onConfirmDifficulty(difficulty);
+  }
+
   return (
     <div className={styles.container}>
       <VisuallyHidden>
@@ -15,13 +27,15 @@ export function DifficultySelect() {
       </VisuallyHidden>
 
       <NativeSelect
+        value={difficulty}
+        onChange={(e) => setDifficulty(e.target.value as Difficulty)}
         label="Difficulty"
         variant="filled"
         description="Higher difficulties feature quicker bosses and tighter timings."
         data={difficulties}
       />
 
-      <Button color="teal" variant="filled" mt="sm" fullWidth>
+      <Button color="teal" variant="filled" mt="sm" fullWidth onClick={handleConfirmDifficulty}>
         Play
       </Button>
     </div>
