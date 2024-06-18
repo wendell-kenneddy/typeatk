deed421e24d8517fe89758c687c5af83f50a93d1
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-18 08:46:35 -0300

    chore: comment StrictMode declaration
    Since it calls functions twice, some bugs appear when using intervals.

--- src/main.tsx
+++ src/main.tsx
@@ -13,9 +13,10 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <MantineProvider defaultColorScheme="dark" theme={theme}>
-      <App />
-    </MantineProvider>
-  </React.StrictMode>
+  // Strict mode messes up timings, be careful when using it
+  // <React.StrictMode>
+  <MantineProvider defaultColorScheme="dark" theme={theme}>
+    <App />
+  </MantineProvider>
+  // </React.StrictMode>
 );
