f97b1d4e45fbd1a7efff887628f05aa41f38f781
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-18 14:03:25 -0300

    feat(App): conditionally render CombatScreen or DifficultySelect

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { CombatScreen } from "./CombatScreen";
 import { DifficultySelect } from "./DifficultySelect";
 import { Header } from "./Header";
+import { Difficulty, difficultyModifiers } from "../data/difficultyModifiers";
 
 export function App() {
+  const [selectedDifficulty, setSelectedDifficulty] = useState<Difficulty | null>(null);
+
   return (
     <>
       <Header />
 
       <main>
-        {/* <DifficultySelect /> */}
-        <CombatScreen totalTime={1000 * 15} wordDamage={15} />
+        {selectedDifficulty ? (
+          <CombatScreen
+            difficultyModifier={difficultyModifiers[selectedDifficulty]}
+            onExitScreen={() => setSelectedDifficulty(null)}
+          />
+        ) : (
+          <DifficultySelect onConfirmDifficulty={(d) => setSelectedDifficulty(d)} />
+        )}
       </main>
     </>
   );
