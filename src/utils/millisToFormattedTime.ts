d8e94098ad16e54fec9b078aa78895b7f0089e77
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-18 14:04:28 -0300

    refactor(millisToFormattedTime): remove unused variables

--- src/utils/millisToFormattedTime.ts
+++ src/utils/millisToFormattedTime.ts
@@ -1,5 +1,3 @@
 export function millisToFormattedTime(milliseconds: number) {
-  const seconds = Math.floor(milliseconds / 1000);
-  const remainder = milliseconds % 1000;
   return `${(milliseconds / 1000).toFixed(2)}`;
 }
