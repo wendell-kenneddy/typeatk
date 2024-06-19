07684ba6a24206c77b25499e1fff4e68c5ce3091
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-19 17:01:29 -0300

    feat(App): add tabs selector including history tab

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,8 @@ import { CombatScreen } from "./CombatScreen";
 import { DifficultySelect } from "./DifficultySelect";
 import { Header } from "./Header";
 import { Difficulty, difficultyModifiers } from "../data/difficultyModifiers";
+import { Tabs } from "@mantine/core";
+import { MatchHistory } from "./MatchHistory";
 
 export function App() {
   const [selectedDifficulty, setSelectedDifficulty] = useState<Difficulty | null>(null);
@@ -18,7 +20,20 @@ export function App() {
             onExitScreen={() => setSelectedDifficulty(null)}
           />
         ) : (
-          <DifficultySelect onConfirmDifficulty={(d) => setSelectedDifficulty(d)} />
+          <Tabs mx="auto" maw={375} mt="md" defaultValue="difficulty-select" color="teal">
+            <Tabs.List flex="true" justify="center">
+              <Tabs.Tab value="difficulty-select">Difficulty</Tabs.Tab>
+              <Tabs.Tab value="match-history">History</Tabs.Tab>
+            </Tabs.List>
+
+            <Tabs.Panel value="difficulty-select">
+              <DifficultySelect onConfirmDifficulty={(d) => setSelectedDifficulty(d)} />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="match-history">
+              <MatchHistory />
+            </Tabs.Panel>
+          </Tabs>
         )}
       </main>
     </>
