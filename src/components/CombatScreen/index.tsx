95ba98a084447f13b8653a901afe7d9a3af63021
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-19 16:58:10 -0300

    feat(CombatScreen): add restart and history saving logic

--- src/components/CombatScreen/index.tsx
+++ src/components/CombatScreen/index.tsx
@@ -1,64 +1,50 @@
-import {
-  Box,
-  Button,
-  Modal,
-  Progress,
-  Stack,
-  Text,
-  TextInput,
-  Title,
-  VisuallyHidden,
-} from "@mantine/core";
+import { Box, Button, Modal, Stack, Text, TextInput, VisuallyHidden } from "@mantine/core";
 import styles from "./CombatScreen.module.css";
 import { useDisclosure } from "@mantine/hooks";
 import { useEffect, useRef, useState } from "react";
-import { millisToFormattedTime } from "../../utils/millisToFormattedTime";
 import { getRandomWord } from "../../utils/getRandomWord";
 import { DifficultyModifier } from "../../data/difficultyModifiers";
+import { ResultScreen } from "../ResultScreen";
+import { CombatProgressDisplay } from "../CombatProgressDisplay";
+import { v4 } from "uuid";
 
 interface CombatScreenProps {
   difficultyModifier: DifficultyModifier;
   onExitScreen: () => void;
 }
 
+export interface MatchResult {
+  id: string;
+  difficultyName: string;
+  finalScore: number;
+  timeScore: number;
+  remainingTime: number;
+  bossScore: number;
+  difficultyMultiplier: number;
+  timestamp: number;
+}
+
 export function CombatScreen({ difficultyModifier, onExitScreen }: CombatScreenProps) {
   const [opened, { close, open }] = useDisclosure(false);
+  const [historySaved, setHistorySaved] = useState(false);
   const [remainingTime, setRemainingTime] = useState(difficultyModifier.totalTime);
   const [targetWord, setTargetWord] = useState("");
   const [typedWord, setTypedWord] = useState("");
   const [bossHp, setBossHp] = useState(100);
-  const intervalRef = useRef(0);
-  const gameEnded = bossHp <= 0 || remainingTime <= 0;
+  const intervalRef = useRef<number | undefined>(undefined);
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const error =
     targetWord && targetWord.substring(0, typedWord.length) != typedWord ? "Wrong word!" : null;
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  function restart() {
-    close();
-    setTargetWord(getRandomWord());
-    setTypedWord("");
-    setBossHp(100);
-    setRemainingTime(difficultyModifier.totalTime);
-    intervalRef.current = setInterval(() => {
-      setRemainingTime((t) => t - 10);
-    }, 10);
-    inputRef.current?.focus();
-  }
-
-  function getTimeScore() {
-    return Math.round((remainingTime / difficultyModifier.totalTime) * 1000);
-  }
-
-  function getBossScore() {
-    return Math.round((1 - bossHp / 100) * 1000);
-  }
+  const gameEnded = bossHp <= 0 || remainingTime <= 0;
 
   useEffect(() => {
     inputRef.current?.focus();
     setTargetWord(getRandomWord());
-    intervalRef.current = setInterval(() => {
-      setRemainingTime((t) => t - 10);
-    }, 10);
+    if (!intervalRef.current) {
+      intervalRef.current = setInterval(() => {
+        setRemainingTime((t) => t - 10);
+      }, 10);
+    }
   }, []);
 
   useEffect(() => {
@@ -80,6 +66,46 @@ export function CombatScreen({ difficultyModifier, onExitScreen }: CombatScreenP
     }
   }, [bossHp, remainingTime]);
 
+  function saveResultToHistory() {
+    if (historySaved) return;
+    const result = getMatchResult();
+    const history: MatchResult[] = JSON.parse(localStorage.getItem("history") ?? "[]");
+    if (history.length == 10) history.shift();
+    history.unshift(result);
+    localStorage.setItem("history", JSON.stringify(history));
+    setHistorySaved(true);
+  }
+
+  function restart() {
+    close();
+    setTargetWord(getRandomWord());
+    setTypedWord("");
+    setBossHp(100);
+    setRemainingTime(difficultyModifier.totalTime);
+    setHistorySaved(false);
+    intervalRef.current = setInterval(() => {
+      setRemainingTime((t) => t - 10);
+    }, 10);
+    inputRef.current?.focus();
+  }
+
+  function getMatchResult(): MatchResult {
+    const { finalScoreMultiplier, totalTime } = difficultyModifier;
+    const timeScore = Math.round((remainingTime / totalTime) * 1000);
+    const bossScore = Math.round((1 - bossHp / 100) * 1000);
+    const finalScore = Math.round((timeScore + bossScore) * finalScoreMultiplier);
+    return {
+      id: v4(),
+      difficultyName: difficultyModifier.name,
+      finalScore,
+      timeScore,
+      bossScore,
+      remainingTime,
+      difficultyMultiplier: finalScoreMultiplier,
+      timestamp: Date.now(),
+    };
+  }
+
   if (!gameEnded && targetWord.length && targetWord == typedWord) {
     setTargetWord(getRandomWord());
     setTypedWord("");
@@ -94,24 +120,11 @@ export function CombatScreen({ difficultyModifier, onExitScreen }: CombatScreenP
           <h2>Now fighting</h2>
         </VisuallyHidden>
 
-        <Stack align="center" justify="center" gap="xs">
-          <Progress
-            aria-label="Remaining time percentage"
-            color="orange"
-            value={(remainingTime / difficultyModifier.totalTime) * 100}
-            transitionDuration={200}
-            w="100%"
-          />
-
-          <Progress
-            w="100%"
-            aria-label="Boss HP"
-            color="red"
-            value={Math.round((bossHp / 100) * 100)}
-          />
-
-          <Text size="sm">{millisToFormattedTime(remainingTime)}</Text>
-        </Stack>
+        <CombatProgressDisplay
+          remainingTime={remainingTime}
+          remainingTimePercentage={(remainingTime / difficultyModifier.totalTime) * 100}
+          remainingBossHpPercentage={bossHp}
+        />
 
         <Box my="md" mx="auto" w="150px" h="150px" bg="teal" />
 
@@ -142,36 +155,7 @@ export function CombatScreen({ difficultyModifier, onExitScreen }: CombatScreenP
         closeOnEscape={false}
       >
         <Stack align="center" gap="xs">
-          <Title c={bossHp <= 0 ? "teal" : "red"} order={3}>
-            {bossHp <= 0 ? "Victory" : "Defeat"}
-          </Title>
-
-          <Stack w="100%" p="xs" gap="xs" className={styles["stats-info"]}>
-            <Text c="teal" size="lg">
-              Score:{" "}
-              {Math.round(
-                (difficultyModifier.bonusScore + getBossScore() + getTimeScore()) *
-                  difficultyModifier.finalScoreMultiplier
-              )}
-            </Text>
-
-            <Text c="orange" size="sm">
-              Remaining time: {millisToFormattedTime(remainingTime)}s ({Math.round(getTimeScore())}
-              pts)
-            </Text>
-
-            <Text c="red" size="sm">
-              Boss score: {getBossScore()}pts
-            </Text>
-
-            <Text c="cyan" size="sm">
-              Difficulty multiplier: {difficultyModifier.finalScoreMultiplier}x
-            </Text>
-
-            <Text c="cyan" size="sm">
-              Bonus: {difficultyModifier.bonusScore}pts
-            </Text>
-          </Stack>
+          <ResultScreen result={getMatchResult()} />
 
           <Stack w="100%" gap="xs">
             <Button w="100%" color="teal" onClick={restart}>
@@ -180,6 +164,10 @@ export function CombatScreen({ difficultyModifier, onExitScreen }: CombatScreenP
 
             <Button w="100%" color="teal" onClick={onExitScreen}>
               Change difficulty
+            </Button>
+
+            <Button disabled={historySaved} w="100%" color="teal" onClick={saveResultToHistory}>
+              {historySaved ? "Already saved" : "Save to history"}
             </Button>
           </Stack>
         </Stack>
