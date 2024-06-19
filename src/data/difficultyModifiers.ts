b2ccdf99e37d8b3e4a12b57442845e67856f56bd
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-19 17:00:02 -0300

    refactor(difficultyModifiers): update DifficultyModifier interface
    Add difficulty name field and remove bonus score field.

--- src/data/difficultyModifiers.ts
+++ src/data/difficultyModifiers.ts
@@ -1,35 +1,35 @@
 export type Difficulty = "easy" | "normal" | "hard";
 
 export interface DifficultyModifier {
+  name: string;
   damagePerWord: number;
   totalTime: number;
   bonusTimePerWord: number;
   finalScoreMultiplier: number;
-  bonusScore: number;
 }
 
 const easy: DifficultyModifier = {
+  name: "Easy",
   damagePerWord: 13,
   totalTime: 23 * 1000,
   bonusTimePerWord: 500,
   finalScoreMultiplier: 0.8,
-  bonusScore: 0,
 };
 
 const normal: DifficultyModifier = {
+  name: "Normal",
   damagePerWord: 9,
   totalTime: 23 * 1000,
   bonusTimePerWord: 100,
   finalScoreMultiplier: 1,
-  bonusScore: 0,
 };
 
 const hard: DifficultyModifier = {
+  name: "Hard",
   damagePerWord: 7,
-  totalTime: 22 * 1000,
+  totalTime: 20 * 1000,
   bonusTimePerWord: 0,
-  finalScoreMultiplier: 1.2,
-  bonusScore: 450,
+  finalScoreMultiplier: 1.35,
 };
 
 export const difficultyModifiers = {
